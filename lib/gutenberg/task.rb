44a146f2dc0726e5dc13dec628be873e0beafd3b
George <somu@so.mu>
2013-10-18 20:16:53 +0400

    Better Gutenberg::Task

--- lib/gutenberg/task.rb
+++ lib/gutenberg/task.rb
@@ -6,18 +6,15 @@ module Gutenberg
   class Task < Rake::TaskLib
     include Rake::DSL if defined? Rake::DSL
 
-    def initialize
+    def initialize(*args)
       desc 'Compile README.md' unless Rake.application.last_comment
       task :readme do
-        original = $stdout.clone
-        $stdout.reopen File.open('README.md', 'w+')
-        require './book/context'
-        $stdout.reopen original
+        Gutenberg::Interface.new(*args).process
       end
 
       desc 'See README.md' unless Rake.application.last_comment
       task :see_readme do
-        require './book/context'
+        Gutenberg::Interface.new(*args).inject
       end
     end
   end
